e8636544cbe450bcfca89badaba5637df9d3030b
Th3R3alMVP <57425855+Th3R3alMVP@users.noreply.github.com>
2025-11-03 03:24:24 +1300

    database is working now

--- CS106/MainWindow.xaml.cs
+++ CS106/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SQLite;
 using Microsoft.Data.Sqlite;
+using System.Data;
 
 namespace CS106;
 
@@ -22,13 +23,45 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
 
-
-
-        var cc = new SQLiteConnection("Data Source=./database/CS106.db");
-        cc.Open();
-        var com = new SQLiteCommand("select * from user", cc);
-        var i = com.ExecuteReader();
-        cc.Close();
-        p.Text = i.ToString();
     }
+    long SignIn(string username, string password)
+    {
+        var database = new SQLiteConnection("Data Source=./database/CS106.db");
+        database.Open();
+        var command = new SQLiteCommand("select employee_id from user where  password is \"" + password + "\" and username is \"" + username + "\"", database);
+        var result = command.ExecuteReader();
+        if (result.Read())
+        {
+            return (long)result[0];
+        }
+
+
+        //p.Visibility = System.Windows.Visibility.Hidden;
+        database.Close();
+        return -1;
+    }
+
+    void CreateUser(string name,string username,string job,float payrate)
+    {
+        var database = new SQLiteConnection("Data Source=./database/CS106.db");
+        database.Open();
+        var command = new SQLiteCommand("insert into employee(name,job_tittle,pay_rate,hire_date,username)" +
+                                                    " VALUES(@name,@job,@payrate,@data,@username)", database);
+        command.Parameters.AddWithValue("@data", DateTime.Now.ToString("dd - MM - yyyy hh: mm:ss.fff"));
+        command.Parameters.AddWithValue("@name", name);
+        command.Parameters.AddWithValue("@username", username);
+        command.Parameters.AddWithValue("@job", job);
+        command.Parameters.AddWithValue("@payrate", payrate);
+        //command.Parameters.AddWithValue("@data", DateTime.Now);
+        var result = command.ExecuteNonQuery();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+            //p.Text = string.Format("{0}", i[0]);
+        p.Text = SignIn(username.Text,password.Password).ToString();
+        CreateUser("new","ddd", "other", 3);
+    }
+
+    
 }
